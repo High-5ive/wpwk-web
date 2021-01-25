3404a1b4f35f6fa0f018dc6408b868cc64bebf65
jusang lee <wntkd0530@naver.com>
2021-01-25 23:26:19 +0900

    기능 : Main에 SCSS 적용, Content.vue 제작

---
+++ src/components/main/Content.vue
@@ -0,0 +1,16 @@
+<template lang="">
+   <div class="content-wrapper">
+      <div class="img-wrapper">
+         <img src="@/assets/img/test1.png" alt="" />
+      </div>
+      <div class="desc-wrapper"></div>
+   </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '@/css/main/content.scss';
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,44 @@
 <template>
    <div class="container">
       여긴 메인 페이지 입니다! Nori 들이 이곳에 있습니다.
+      <div v-for="(ctt, idx) in cttList" :key="idx">
+         <nori-content></nori-content>
+         <br />
+      </div>
    </div>
 </template>
 
 <script>
-export default {};
+import NoriContent from '@/components/main/Content.vue';
+export default {
+   components: {
+      NoriContent,
+   },
+   data() {
+      return {
+         cttList: [
+            {
+               index: '3',
+               title: 'sample',
+            },
+            {
+               index: '3',
+               title: 'sample',
+            },
+            {
+               index: '3',
+               title: 'sample',
+            },
+            {
+               index: '3',
+               title: 'sample',
+            },
+         ],
+      };
+   },
+};
 </script>
 
-<style scoped>
-.container {
-   background-color: red;
-}
+<style lang="scss">
+@import '@/css/main.scss';
 </style>
