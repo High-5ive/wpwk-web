11595c755119028b80287b6d590aaa9e1b25faa5
jusang lee <wntkd0530@naver.com>
2021-01-26 14:25:25 +0900

    디자인 : 노리컨텐츠 추가, SCSS 적용

---
+++ src/components/main/NoriContent.vue
@@ -0,0 +1,30 @@
+<template lang="">
+   <div class="content-wrapper">
+      <div class="img-wrapper">
+         <img src="@/assets/img/test1.png" alt="" />
+      </div>
+      <div class="desc-wrapper">
+         <p class="title">{{ sendNori.title }}</p>
+         아이템 갯수 : {{ sendNori.itemList.length }}
+         <p>{{ sendNori.time }}</p>
+         <span v-for="(cate, idx) in sendNori.selectedCategories" :key="idx">
+            {{ cate }}
+         </span>
+         <br />
+         <span v-for="(tag, idx) in sendNori.hashtags" :key="'tag' + idx">
+            {{ tag }}
+         </span>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'NoriContent',
+   props: ['sendNori'],
+};
+</script>
+
+<style lang="scss">
+@import '@/css/main/noriContent.scss';
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,40 +1,46 @@
 <template>
    <div class="container">
-      여긴 메인 페이지 입니다! Nori 들이 이곳에 있습니다.
-      <div v-for="(ctt, idx) in cttList" :key="idx">
-         <nori-content></nori-content>
+      <div v-for="(nori, idx) in NoriList" :key="idx">
+         <nori-content :sendNori="nori"></nori-content>
          <br />
       </div>
    </div>
 </template>
 
 <script>
-import NoriContent from '@/components/main/Content.vue';
+import NoriContent from '@/components/main/NoriContent.vue';
+
+const dummyInsert = (list) => {
+   const nori = {
+      title: '테스트입니당',
+      itemList: [1, 2],
+      time: {
+         hour: 2,
+         minute: 0,
+      },
+      selectedCategories: ['공간지능', '수리지능'],
+      hashtags: ['아이가참_좋아해', '간단간단'],
+   };
+
+   var i = 1;
+   while (i++ < 5) {
+      list.push(nori);
+   }
+
+   console.log(nori);
+};
+
 export default {
    components: {
       NoriContent,
    },
    data() {
       return {
-         cttList: [
-            {
-               index: '3',
-               title: 'sample',
-            },
-            {
-               index: '3',
-               title: 'sample',
-            },
-            {
-               index: '3',
-               title: 'sample',
-            },
-            {
-               index: '3',
-               title: 'sample',
-            },
-         ],
+         NoriList: [],
       };
+   },
+   created() {
+      dummyInsert(this.NoriList);
    },
 };
 </script>
