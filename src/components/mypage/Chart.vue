0ac7c8791eeff37d1dc860fc0f4993d81b3816f1
Sujin Kim <sj_3100@daum.net>
2021-02-01 17:55:34 +0900

    기능: 마이페이지 차트 구현

---
+++ src/components/mypage/Chart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="width:800px;">
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+<script>
+import Chart from 'chart.js'
+import myChartData from '@/assets/js/chartData.js'
+
+export default {
+  name: "Chart",
+  data: function () {
+    return {
+      chartData: myChartData
+    }
+  },
+  methods: {
+    createChart: function (charId, chartData) {
+      const ctx = document.getElementById(charId)
+      const myChart = new Chart (ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options,
+      })
+      console.log(myChart)
+    }
+  },
+  mounted () {
+    this.createChart('myChart', this.chartData)
+  }
+}
+</script>
+<style lang="">
+  
+</style>
--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,11 +1,45 @@
 <template>
-   <div class="container">
-      여긴 마이 페이지 입니다!
+   <div class="container d-flex flex-column align-center">
+      <div class="top">
+         <span>{{ userInfo.nickname }}</span>
+         
+      </div>
+      <div class="middle d-flex justify-space-around">
+         <div class="left">
+            작성 글
+         </div>
+         <div class="center">
+            작성 노리
+         </div>
+         <div class="right">
+            관심 노리
+         </div>
+      </div>
+      <div class="bottom">
+         <chart />
+      </div>
    </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+import Chart from '@/components/mypage/Chart.vue'
 
-<style scoped>
+export default {
+   name: "Mypage",
+   components: {
+      Chart,
+   },
+   computed: {
+      ...mapState(['userInfo']),
+   },
+  
+}
+</script>
+<style lang="scss" scoped>
 .container {
-   background-color: blue;
+   width: 100%;
+   .middle {
+      width: 100%;
+   }
 }
 </style>
