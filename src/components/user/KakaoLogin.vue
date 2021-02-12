f62795f1ebb9a505e753d8a0e2528cb54cbee628
김한솔 <doingnow94@gmail.com>
2021-02-12 22:56:27 +0900

    # 배포 시 config 변경

--- src/components/user/KakaoLogin.vue
+++ src/components/user/KakaoLogin.vue
@@ -1,7 +1,10 @@
 <template lang="">
   <div class="kakao">
-    <a
+    <!-- <a
       href="https://kauth.kakao.com/oauth/authorize?client_id=f871952e3d6294ff03c50c4e129fa152&redirect_uri=http://localhost:8080/kakao&response_type=code"
+    > -->
+    <a
+      href="https://kauth.kakao.com/oauth/authorize?client_id=f871952e3d6294ff03c50c4e129fa152&redirect_uri=http://i4a205.p.ssafy.io/kakao&response_type=code"
     >
       <img class="kakao-button" src="@/assets/kakaoButton.png"/>
     </a>
--- src/components/user/NaverLogin.vue
+++ src/components/user/NaverLogin.vue
@@ -3,11 +3,13 @@
 </template>
 <script>
 const naverService = () => {
+
    var naverLogin = new window.naver.LoginWithNaverId({
       clientId: 'dYxRjRrd569zhsnI7b3z',
-      callbackUrl: 'http://localhost:8080/login/oauth2/code/naver',
+      // callbackUrl: 'http://localhost:8080/login/oauth2/code/naver',
+      callbackUrl: 'http://i4a205.p.ssafy.io/login/oauth2/code/naver',
       isPopup: false /* 팝업을 통한 연동처리 여부 */,
-      loginButton: { color: 'green', type: 1, height: 40 } /* 로그인 버튼의 타입을 지정 */,
+      loginButton: { color: 'green', type: 1, height: 40 } /* 로그인 버튼의 타입을 지정 */,      
    });
 
    /* 설정정보를 초기화하고 연동을 준비 */
