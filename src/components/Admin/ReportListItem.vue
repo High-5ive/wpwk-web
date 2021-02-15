6306dac029187a7d28ccc01e3e40dc93dbccd033
TaeSeongMin <goji7309@gmail.com>
2021-02-15 21:02:30 +0900

    # 컨텐츠 뷰 요청시 URL 에 객체 파라미터로 넘기지 않고 ID만 넘기는 것으로 구현

--- src/components/Admin/ReportListItem.vue
+++ src/components/Admin/ReportListItem.vue
@@ -1,114 +1,123 @@
 <template>
-    <div class="report-wrapper">
-        <div class="report-content-wrapper">
-            <div class="reporter-creator">
-                <div class="reporter"> {{ this.reporter }} </div>
-                <i class="fas fa-hand-point-right"></i>
-                <div class="creator"> {{ this.creator }} </div>
+  <div class="report-wrapper">
+    <div class="report-content-wrapper">
+      <div class="reporter-creator">
+        <div class="reporter">{{ this.reporter }}</div>
+        <i class="fas fa-hand-point-right"></i>
+        <div class="creator">{{ this.creator }}</div>
 
-                <!-- modal로 신고 받은 페이지 띄우기 -->
-                <div class="page">
-                    <!-- modal로 신고 받은 페이지 띄우기 -->
-                    <v-dialog v-model="dialog" persistent width="400px">
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-btn outlined rounded small color="primary" v-bind="attrs" v-on="on"> 컨텐츠 확인 </v-btn>
-                        </template>
-                        <div>
-                            <!-- 닫기 버튼 -->
-                            <v-btn color="red" dark rounded @click="dialog = false"> <v-icon>mdi-close</v-icon> </v-btn>
-                        </div>
-                        <iframe width="350px" height="620px" :src="url"> </iframe>
-                    </v-dialog>
-                </div>
+        <!-- modal로 신고 받은 페이지 띄우기 -->
+        <div class="page">
+          <!-- modal로 신고 받은 페이지 띄우기 -->
+          <v-dialog v-model="dialog" persistent width="400px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                outlined
+                rounded
+                small
+                color="primary"
+                v-bind="attrs"
+                v-on="on"
+              >
+                컨텐츠 확인
+              </v-btn>
+            </template>
+            <div>
+              <!-- 닫기 버튼 -->
+              <v-btn color="red" dark rounded @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
             </div>
-
-            <div class="contents-title nf nf-600">
-                {{ this.title }}
-            </div>
-
-            <div class="reason nf">
-                신고 사유: {{ this.comment }}
-            </div>
-   
-            <!-- 신고 처리 버튼 -->
-            <div v-if="done" class="done nf nf-600"> {{ this.choose }} 처리가 완료됐습니다. </div>
-            <div v-else class="buttons">
-                <v-btn outlined rounded small @click="waitContents"> 문제없음 </v-btn>
-                <v-btn outlined rounded small @click="warnContents"> 경고 </v-btn>
-                <v-btn outlined rounded small @click="deleteContents"> 삭제 </v-btn>
-            </div>
-            
+            <iframe width="350px" height="620px" :src="url"> </iframe>
+          </v-dialog>
         </div>
+      </div>
+
+      <div class="contents-title nf nf-600">
+        {{ this.title }}
+      </div>
+
+      <div class="reason nf">신고 사유: {{ this.comment }}</div>
+
+      <!-- 신고 처리 버튼 -->
+      <div v-if="done" class="done nf nf-600">
+        {{ this.choose }} 처리가 완료됐습니다.
+      </div>
+      <div v-else class="buttons">
+        <v-btn outlined rounded small @click="waitContents"> 문제없음 </v-btn>
+        <v-btn outlined rounded small @click="warnContents"> 경고 </v-btn>
+        <v-btn outlined rounded small @click="deleteContents"> 삭제 </v-btn>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ReportListItem",
-    props: {
-        report: Object,
+  name: 'ReportListItem',
+  props: {
+    report: Object,
+  },
+  data: function() {
+    return {
+      creator: '',
+      reporter: '',
+      title: '',
+      contentsId: 0,
+      comment: '',
+      dialog: false,
+      url: '',
+      choose: '',
+      done: '',
+    };
+  },
+  methods: {
+    getReport: function() {
+      this.creator = this.report.creator;
+      this.reporter = this.report.reporter;
+      this.title = this.report.title;
+      this.contentsId = this.report.contentsId;
+      this.comment = this.report.comment;
+      this.done = this.choose;
     },
-    data: function () {
-        return {
-            creator: "",
-            reporter: "",
-            title: "",
-            contentsId: 0,
-            comment: "",
-            dialog: false,
-            url: "",
-            choose : "",
-            done: ""
-        }
+    waitContents: function() {
+      var wait = confirm('문제없음 처리 하시겠습니까?');
+      if (wait) {
+        this.choose = '문제없음';
+      }
     },
-    methods: {
-        getReport: function () {
-            this.creator = this.report.creator
-            this.reporter = this.report.reporter
-            this.title = this.report.title
-            this.contentsId = this.report.contentsId
-            this.comment = this.report.comment
-            this.done = this.choose
-        },
-        waitContents: function () {
-            var wait = confirm('문제없음 처리 하시겠습니까?')
-            if (wait) {
-                this.choose = "문제없음"
-            }
-        },
-        warnContents: function () {
-            var warn = confirm('경고 처리 하시겠습니까?')
-            if (warn) {
-                this.choose = "경고"
-            }
-            // 게시글 작성자에게 경고 알림 전송
-        },
-        deleteContents: function () {
-            var deleteC = confirm('삭제 처리 하시겠습니까?')
-            if (deleteC) {
-                this.choose = "삭제"
-            }
-            // 게시글 작성자에게 경고 알림 전송
-        }
+    warnContents: function() {
+      var warn = confirm('경고 처리 하시겠습니까?');
+      if (warn) {
+        this.choose = '경고';
+      }
+      // 게시글 작성자에게 경고 알림 전송
     },
-    created: function () {
-        this.getReport()
-        // 임의의 값 입력
-        this.url = "http://localhost:8080/view"
+    deleteContents: function() {
+      var deleteC = confirm('삭제 처리 하시겠습니까?');
+      if (deleteC) {
+        this.choose = '삭제';
+      }
+      // 게시글 작성자에게 경고 알림 전송
     },
-    watch: {
-        choose: function () {
-            this.getReport()
-        }
+  },
+  created: function() {
+    this.getReport();
+    // 임의의 값 입력
+    this.url = 'http://localhost:8080/view';
+  },
+  watch: {
+    choose: function() {
+      this.getReport();
     },
-    // computed: {
-    //     getUrl: function () {
-    //         // contentsId 받아서 해당 페이지 로드
-    //         this.url = `http://localhost:8080/view/${this.contentsId}`
-    //     }
-    // }
-}
+  },
+  // computed: {
+  //     getUrl: function () {
+  //         // contentsId 받아서 해당 페이지 로드
+  //         this.url = `http://localhost:8080/view/${this.contentsId}`
+  //     }
+  // }
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/main/NoriContent.vue
+++ src/components/main/NoriContent.vue
@@ -1,35 +1,45 @@
 <template lang="">
-   <!-- 전체 한 덩어리 -->
-   <div class="content-wrapper">
-      <!-- 이미지썸네일과 뱃지들 -->
-      <div class="img-wrapper">
-         <img :src="thumbnail" alt="" @click="contentsClick(sendNori)" />
-         <!-- 이미지 위에 표시되는 뱃지들이 위치합니다 -->
-         <!-- <div class="itemLength">{{ sendNori.itemList.length }}</div> -->
-         <div class="badge-cate">
-            <span v-for="(ability, idx) in sendNori.abilities" :key="idx" @click="categorySearch(ability)">
-               {{ ability }}
-            </span>
-         </div>
+  <!-- 전체 한 덩어리 -->
+  <div class="content-wrapper">
+    <!-- 이미지썸네일과 뱃지들 -->
+    <div class="img-wrapper">
+      <img :src="thumbnail" alt="" @click="contentsClick(sendNori)" />
+      <!-- 이미지 위에 표시되는 뱃지들이 위치합니다 -->
+      <!-- <div class="itemLength">{{ sendNori.itemList.length }}</div> -->
+      <div class="badge-cate">
+        <span
+          v-for="(ability, idx) in sendNori.abilities"
+          :key="idx"
+          @click="categorySearch(ability)"
+        >
+          {{ ability }}
+        </span>
       </div>
+    </div>
 
-      <!-- 하단 설명글 -->
-      <div class="desc-wrapper">
-         <h1>{{ sendNori.title }}</h1>
-         <span v-for="(tag, idx) in sendNori.tagList" :key="'tag' + idx" @click="tagSearch(tag.name)"> #{{ tag.name }} </span>
-         <h6>{{ sendNori.nickname }}</h6>
-         <div v-if="sendNori.favorite" class="btn-like" @click="doUnLike">
-            <v-icon>
-               mdi-heart
-            </v-icon>
-         </div>
-         <div v-if="!sendNori.favorite" class="btn-unlike" @click="doLike">
-            <v-icon>
-               mdi-heart
-            </v-icon>
-         </div>
+    <!-- 하단 설명글 -->
+    <div class="desc-wrapper">
+      <h1>{{ sendNori.title }}</h1>
+      <span
+        v-for="(tag, idx) in sendNori.tagList"
+        :key="'tag' + idx"
+        @click="tagSearch(tag.name)"
+      >
+        #{{ tag.name }}
+      </span>
+      <h6>{{ sendNori.nickname }}</h6>
+      <div v-if="sendNori.favorite" class="btn-like" @click="doUnLike">
+        <v-icon>
+          mdi-heart
+        </v-icon>
       </div>
-   </div>
+      <div v-if="!sendNori.favorite" class="btn-unlike" @click="doLike">
+        <v-icon>
+          mdi-heart
+        </v-icon>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -37,76 +47,91 @@ import router from '../../router/router';
 import { favoriteContents, unFavoriteContents } from '@/api/contents.js';
 
 export default {
-   name: 'NoriContent',
-   props: ['sendNori'],
-   data() {
-      return {
-         thumbnail: '',
-         categories: ['언어지능', '논리수학지능', '음악지능', '신체운동지능', '공간지능', '자연지능', '대인지능', '개인내지능'],
-      };
-   },
+  name: 'NoriContent',
+  props: ['sendNori'],
+  data() {
+    return {
+      thumbnail: '',
+      categories: [
+        '언어지능',
+        '논리수학지능',
+        '음악지능',
+        '신체운동지능',
+        '공간지능',
+        '자연지능',
+        '대인지능',
+        '개인내지능',
+      ],
+    };
+  },
 
-   methods: {
-      contentsClick: function(target) {
-         router.push({ name: 'ContentsView', params: { nori: target } });
-      },
-      tagSearch: function(tagName) {
-         router.push({ name: 'SearchResult', params: { searchValue: tagName, type: 'tag' } });
-         // this.$emit('tagEvent');
-      },
-      categorySearch: function(category) {
-         for (var i = 0; i < this.categories.length; i++) {
-            if (category === this.categories[i]) {
-               this.$router.push({ name: 'SearchResult', params: { searchValue: i + 1, type: 'category' } });
-            }
-         }
-      },
-      doLike: function() {
-         var id = {
-            id: this.sendNori.id,
-         };
-
-         favoriteContents(
-            id,
-            () => {
-               this.sendNori.favorite = true;
-            },
-            (err) => {
-               console.log(err);
-            }
-         );
-      },
-      doUnLike: function() {
-         unFavoriteContents(
-            this.sendNori.id,
-            () => {
-               this.sendNori.favorite = false;
-            },
-            (err) => {
-               console.log(err);
-            }
-         );
-      },
-   },
-
-   mounted() {
-      //이미지 없을 경우, 기본 이미지
-      if (this.sendNori.thumb == null) {
-         this.thumbnail = require('@/assets/cv-bg.png');
-      } else {
-         this.thumbnail = this.sendNori.thumb;
+  methods: {
+    contentsClick: function(target) {
+      router.push({ name: 'ContentsView', params: { id: target.id } });
+    },
+    tagSearch: function(tagName) {
+      router.push({
+        name: 'SearchResult',
+        params: { searchValue: tagName, type: 'tag' },
+      });
+      // this.$emit('tagEvent');
+    },
+    categorySearch: function(category) {
+      for (var i = 0; i < this.categories.length; i++) {
+        if (category === this.categories[i]) {
+          this.$router.push({
+            name: 'SearchResult',
+            params: { searchValue: i + 1, type: 'category' },
+          });
+        }
       }
-   },
+    },
+    doLike: function() {
+      var id = {
+        id: this.sendNori.id,
+      };
 
-   watch: {
-      sendNori: function() {
-         if (this.sendNori.thumb == null) {
-            this.thumbnail = require('@/assets/cv-bg.png');
-         } else {
-            this.thumbnail = this.sendNori.thumb;
-         }
-      },
-   },
+      favoriteContents(
+        id,
+        () => {
+          this.sendNori.favorite = true;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+    doUnLike: function() {
+      unFavoriteContents(
+        this.sendNori.id,
+        () => {
+          this.sendNori.favorite = false;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+  },
+
+  mounted() {
+    //이미지 없을 경우, 기본 이미지
+    if (this.sendNori.thumb == null) {
+      this.thumbnail = require('@/assets/cv-bg.png');
+    } else {
+      this.thumbnail = this.sendNori.thumb;
+    }
+  },
+
+  watch: {
+    sendNori: function() {
+      if (this.sendNori.thumb == null) {
+        this.thumbnail = require('@/assets/cv-bg.png');
+      } else {
+        this.thumbnail = this.sendNori.thumb;
+      }
+    },
+  },
 };
 </script>
 
