2a5d1efccb1ec3becb21444932263661f5bd5853
김수연 <xsw7563@naver.com>
2021-02-17 01:06:33 +0900

    기능 : 알림 타입 별 아이콘 구현

---
+++ src/components/main/NotificationItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="icon-message">
+      <!-- <v-icon> {{ icon }} </v-icon> -->
+      <v-icon v-if="type =='ADMIN_MESSAGE'" class='icon1'> mdi-comment-text-outline </v-icon>
+      <v-icon v-else-if="type =='DELETE'" class='icon2'> mdi-delete-alert </v-icon>
+      <v-icon v-else-if="type =='WARN'" class='icon3'> mdi-alert </v-icon>
+      <v-icon v-else-if="type =='LIKE'" class='icon4'> mdi-account-heart </v-icon>
+      <v-icon v-else-if="type =='COMMENT'" class='icon5'> mdi-comment-edit </v-icon>
+      <v-icon v-else class='icon6'> mdi-account-arrow-left </v-icon>
+      {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NotificationItem',
+    data: function () {
+        return {
+            type: '',
+            message: '',
+            // icon: '',
+        }
+    },
+    methods: {
+        getNotification: function () {
+            this.type = this.notification.messageType
+            this.message = this.notification.message
+        },
+        // getIcon: function() {
+        //     switch(this.type) {
+        //         case 'ADMIN_MESSAGE':
+        //             this.icon = 'mdi-comment-text-outline'
+        //             break;
+        //         case 'DELETE' :
+        //             this.icon = 'mdi-delete-alert'
+        //             break;
+        //         case 'WARN' :
+        //             this.icon = 'mdi-alert'
+        //             break;
+        //         case 'LIKE' :
+        //             this.icon ='mdi-account-heart'
+        //             break;
+        //         case 'COMMENT' :
+        //             this.icon = 'mdi-comment-edit'
+        //             break;
+        //         case 'FOLLOW' :
+        //             this.icon = 'mdi-account-arrow-left'
+        //             break;
+        //     }
+        // }
+    },
+    props: {
+        notification: Object,
+    },
+    created: function() {
+        this.getNotification()
+        // this.getIcon()
+    }
+}
+</script>
+
+<style>
+
+</style>
